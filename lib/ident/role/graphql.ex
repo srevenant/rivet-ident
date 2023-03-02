bd169b16b937b5462a1d33af26cbbdc47f50abda
Lyle Mantooth <lyle@menteeth.us>
2023-03-02 15:08:54 -0500

    Add User Graphql schema.
    Should some of these things be broken out to other Ident modules? Seems
    like no benefit for a bunch of work.

--- lib/ident/role/graphql.ex
+++ lib/ident/role/graphql.ex
@@ -1,3 +1,20 @@
 defmodule Rivet.Data.Ident.Role.Graphql do
   @moduledoc false
+  use Absinthe.Schema.Notation
+  import Rivet.Graphql
+  import Rivet.Data.Ident.Role.Resolver
+
+  @desc "Authorization Ident.Role"
+  object :role do
+    field(:id, non_null(:integer))
+    field(:name, non_null(:string))
+    field(:description, non_null(:string))
+  end
+
+  object :role_queries do
+    field :user_roles, list_of(:role) do
+      arg(:name, :string)
+      resolve(&query_roles/2)
+    end
+  end
 end
--- lib/ident/role/resolver.ex
+++ lib/ident/role/resolver.ex
@@ -1,3 +1,20 @@
 defmodule Rivet.Data.Ident.Role.Resolver do
   @moduledoc false
+  alias Rivet.Auth
+  alias Rivet.Data.Ident
+
+  def query_roles(%{name: name}, info) do
+    with {:ok, _} <- authz_action(info, %Auth.Assertion{action: :user_admin}, "listRoles"),
+         {:ok, role} <- Ident.Role.one(name: name) do
+      {:ok, [role]}
+    else
+      _ -> {:error, "cannot find role: #{name}"}
+    end
+  end
+
+  def query_roles(_args, info) do
+    with {:ok, _} <- authz_action(info, %Auth.Assertion{action: :user_admin}, "listRoles") do
+      Ident.Role.all()
+    end
+  end
 end
