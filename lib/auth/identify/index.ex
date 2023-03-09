35e714edbb9ee6e80c401f07c25a3332a70167b0
Brandon Gillespie <bjg@solv.com>
2023-03-09 08:45:05 -0700

    identify

---
+++ lib/auth/identify/index.ex
@@ -0,0 +1,25 @@
+defmodule Rivet.Auth.Identify do
+  @moduledoc """
+  """
+  alias Rivet.{Ident, Auth}
+
+  ###########################################################################
+  @doc """
+  """
+  @spec identify(String.t(), map()) :: {:ok | :error, Auth.Domain.t()} | {:exists, %Ident.Email{}}
+  def identify(hostname, %{email: eaddr}) do
+    case Ident.Email.all!(address: eaddr) do
+      [%Ident.Email{} = email] ->
+        {:exists, email}
+
+      _other ->
+        # create new user profile with identity info only
+        Ident.User.Lib.signup_only_identity(%Auth.Domain{
+          hostname: hostname,
+          input: %{email: %{address: eaddr, verified: false}, settings: %{}}
+        })
+    end
+  end
+
+  def identify(_, _), do: :error
+end
