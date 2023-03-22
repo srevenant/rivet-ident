ad18be0933b509e4c25d2e8479d118de67f77952
Brandon Gillespie <bjg@solv.com>
2023-03-22 12:49:53 -0600

    .

--- lib/ident/user/lib/index.ex
+++ lib/ident/user/lib/index.ex
@@ -5,25 +5,18 @@ defmodule Rivet.Ident.User.Lib do
   alias Rivet.Auth
   require Logger
 
-  def search(%{matching: matching, limit: limit}) do
-    {:ok,
-     @repo.all(
-       from(u in Ident.User,
-         join: e in Ident.Email,
-         where: e.user_id == u.id,
-         join: h in Ident.Handle,
-         where: h.user_id == u.id,
-         where:
-           like(u.name, ^matching) or
-             like(h.handle, ^matching) or
-             like(e.address, ^matching),
-         limit: ^limit,
-         distinct: u.id
-       )
-     )}
-  rescue
-    error ->
-      {:error, error}
+  def search(%{matching: match}, args) do
+   match = String.downcase(Regex.replace(~r/[^a-z0-9]/i, match, ""))
+
+   from(u in Ident.User,
+     join: e in Ident.UserEmail,
+     on: e.user_id == u.id,
+     join: h in Ident.UserHandle,
+     on: h.user_id == u.id,
+     where: like(u.name, ^match) or like(h.handle, ^match) or like(e.address, ^match)
+   )
+   |> Rivet.Ecto.Collection.enrich_query_args(args)
+   |> Rivet.Ident.User.all()
   end
 
   @doc """
