142df40310055e3c66eaa03222db8ef1669a4722
Lyle Mantooth <lyle@menteeth.us>
2023-05-11 11:47:48 -0400

    Load email templates from database if provided.

--- lib/ident/user/notify/verification.ex
+++ lib/ident/user/notify/verification.ex
@@ -20,22 +20,25 @@ defmodule Rivet.Ident.User.Notify.Verification do
     end
   end
 
-  @behaviour Rivet.Ecto.Collection
   @impl Rivet.Email.Template
-  def generate(%Ident.Email{}, attr) do
-    {:ok, "#{attr.org} email verification",
-     """
-     <p/>
-     This email was added to an account at #{attr.org}.  However, it is not yet verified.  Please verify this email address by clicking the Verify link:
-     <p/>
-     <a href="#{attr.link_back}/ev?code=#{attr.code}">Verify</a>
-     <p/>
-     If you are unable to view or click the link in this message, copy the following URL and paste it in your browser:
-     <p/><code>#{attr.link_back}/ev?code=#{attr.code}</code>
-     <p/>
-     This verification code will expire in 1 day.
-     <p/>
-     #{attr.email_sig}
-     """}
+  def generate(%Ident.Email{} = email, attr) do
+    with {:error, error} <- load(email, attr) do
+      Logger.warn(error)
+
+      {:ok, "#{attr.org} email verification",
+       """
+       <p/>
+       This email was added to an account at #{attr.org}.  However, it is not yet verified.  Please verify this email address by clicking the Verify link:
+       <p/>
+       <a href="#{attr.link_back}/ev?code=#{attr.code}">Verify</a>
+       <p/>
+       If you are unable to view or click the link in this message, copy the following URL and paste it in your browser:
+       <p/><code>#{attr.link_back}/ev?code=#{attr.code}</code>
+       <p/>
+       This verification code will expire in 1 day.
+       <p/>
+       #{attr.email_sig}
+       """}
+    end
   end
 end
