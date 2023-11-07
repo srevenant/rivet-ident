203f30d40bc96ba8f4e9a032b0f4dcb75fd5fc81
Brandon Gillespie <bjg@solv.com>
2023-11-07 08:37:01 -0700

    checkpoint

--- lib/ident/context.ex
+++ lib/ident/context.ex
@@ -1,22 +1,22 @@
 defmodule Rivet.Ident.Context do
   defmacro __using__(_) do
     quote location: :keep do
-      @notify_password_changed Application.compile_env(
+      @notify_password_changed Application.get_env(
                                  :rivet_ident,
                                  :notify_password_changed,
                                  Rivet.Ident.User.Notify.PasswodChanged
                                )
-      @notify_password_reset Application.compile_env(
+      @notify_password_reset Application.get_env(
                                :rivet_ident,
                                :notify_password_reset,
                                Rivet.Ident.User.Notify.Passwordeset
                              )
-      @notify_user_failed_change Application.compile_env(
+      @notify_user_failed_change Application.get_env(
                                    :rivet_ident,
                                    :notify_user_failed_change,
                                    Rivet.Ident.User.Notify.FaildChange
                                  )
-      @notify_user_verification Application.compile_env(
+      @notify_user_verification Application.get_env(
                                   :rivet_ident,
                                   :notify_user_verification,
                                   Rivet.Ident.User.Notify.Verifcation
