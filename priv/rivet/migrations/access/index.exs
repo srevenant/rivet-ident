573627f15716fb298b2a74af29c395282cc6f6ca
Lyle Mantooth <lyle@menteeth.us>
2023-11-08 10:22:39 -0500

    Activate Access ChangeDomain migration.

--- priv/rivet/migrations/access/index.exs
+++ priv/rivet/migrations/access/index.exs
@@ -1,5 +1,5 @@
 alias Rivet.Ident.Access.Migrations, as: M
 [
+  [base: true, version: 10, module: M.ChangeDomain],
   [base: true, version: 0, module: M.Base]
-  # [base: true, version: 10, module: M.ChangeDomain],
 ]
