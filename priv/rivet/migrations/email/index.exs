a6dc443421d11c1ec927e80e53dacb9eef2e10ba
Brandon Gillespie <bjg@solv.com>
2024-04-22 15:30:53 -0600

    checkpoint

--- priv/rivet/migrations/email/index.exs
+++ priv/rivet/migrations/email/index.exs
@@ -1,7 +1,5 @@
 alias Rivet.Ident.Email.Migrations, as: M
 [
-  [
-    base: true, version: 0, module: M.Base,
-    version: 100, module: M.Bounce
-  ]
+  [ base: true, version: 0, module: M.Base],
+  [ version: 1, module: M.Bounce ]
 ]
