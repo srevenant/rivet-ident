40b45389a48eef33e22b12e19e5737833002fa92
Brandon Gillespie <brandon@cold.org>
2026-07-20 13:14:13 -0600

    .

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RivetIdent.MixProject do
   def project do
     [
       app: :rivet_ident,
-      version: "4.0.0",
+      version: "4.0.1",
       description: "Authentication and Authorization add-on for Rivets Framework",
       source_url: @source_url,
       package: package(),
